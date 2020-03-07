c42ea094409f467a1ce8e0e176cdc8024979cd1d
DeejayRevok <seryi_one@hotmail.com>
2020-03-07 21:27:20 +0100

    Fix some pylint issues

--- event_manager/services/uaa_service.py
+++ event_manager/services/uaa_service.py
@@ -1,17 +1,40 @@
+"""
+Module which implements the service used to interact with the UAA service
+"""
 from aiohttp import ClientSession
 from aiohttp.web_exceptions import HTTPUnauthorized
 
 
 class UaaService:
+    """
+    Class used to interact with the UAA service
+    """
     URL_BASE = '{protocol}://{host}:{port}/{path}'
     PATHS = {'validate_token': 'v1/api/auth/token'}
 
-    def __init__(self, protocol, host, port):
+    def __init__(self, protocol: str, host: str, port: str):
+        """
+        Initialize the service with the required parameters to connect to the UAA
+
+        Args:
+            protocol: communications protocol
+            host: UAA service host address
+            port: UAA service port
+        """
         self._protocol = protocol
         self._host = host
         self._port = int(port)
 
-    async def validate_token(self, token):
+    async def validate_token(self, token: str) -> dict:
+        """
+        Request the JWT token validation to the associated UAA service
+
+        Args:
+            token: JWT token to validate
+
+        Returns: user authenticated if the token is valid
+
+        """
         async with ClientSession() as client:
             fetch_user_url = self.URL_BASE.format(protocol=self._protocol, host=self._host, port=self._port,
                                                   path=self.PATHS['validate_token'])
