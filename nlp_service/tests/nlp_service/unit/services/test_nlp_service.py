c99a349f1e0017e8b92a43c86f9a090ea5836d7a
Sergio Díez Sanz <sergio.diez@taiger.com>
2020-04-19 18:43:33 +0200

    Fix nlp service tests Solve lint issues for news manager

--- nlp_service/tests/nlp_service/unit/services/test_nlp_service.py
+++ nlp_service/tests/nlp_service/unit/services/test_nlp_service.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from unittest.mock import patch, MagicMock, call
 
 from aiounittest import async_test
-from news_service_lib.models import NamedEntity, New
+from news_service_lib.models import New
 
 from nlp_service.services.nlp_service import NlpService
 
@@ -11,16 +11,28 @@ class TestNlpService(TestCase):
 
     TEST_NEW = New(title='test_title', content='test_content', date=2124124.0, categories=['test_category'])
     TEST_TEXT = 'test_text'
-    TEST_ENTITIES = [NamedEntity(text='test_entity_text', type='test_entity_type')]
+    TEST_ENTITY = ('test_entity_text', 'test_entity_type')
+    TEST_SENTENCES = ['test_sentence_1', 'test_sentence_2']
 
     @patch('nlp_service.services.nlp_service.Pipeline')
     @async_test
-    async def test_get_named_entities(self, mocked_nlp_pipeline):
+    async def test_process_text(self, mocked_nlp_pipeline):
+        mock_first_sentence = MagicMock()
+        mock_first_sentence.text = self.TEST_SENTENCES[0]
+        mock_second_sentence = MagicMock()
+        mock_second_sentence.text = self.TEST_SENTENCES[1]
+
+        mock_entity = MagicMock()
+        mock_entity.text = self.TEST_ENTITY[0]
+        mock_entity.type = self.TEST_ENTITY[1]
+
         nlp_service = NlpService()
-        mocked_nlp_pipeline()().entities = self.TEST_ENTITIES
-        entities = list(await nlp_service.get_named_entities(self.TEST_TEXT))
+        mocked_nlp_pipeline()().entities = [mock_entity]
+        mocked_nlp_pipeline()().sentences = [mock_first_sentence, mock_second_sentence]
+        nlp_doc = await nlp_service.get_processed_text(self.TEST_TEXT)
         mocked_nlp_pipeline().assert_called_with(self.TEST_TEXT)
-        self.assertEqual(len(entities), 1)
+        self.assertListEqual(nlp_doc.named_entities, [self.TEST_ENTITY])
+        self.assertListEqual(nlp_doc.sentences, self.TEST_SENTENCES)
 
     @patch('nlp_service.services.nlp_service.publish_hydrated_new')
     @patch('nlp_service.services.nlp_service.hydrate_new_with_entities')
@@ -35,6 +47,6 @@ class TestNlpService(TestCase):
         await nlp_service.hydrate_new(dict(self.TEST_NEW))
         nlp_service.CELERY_NLP_PIPELINE[0].s.assert_called_with(dict(self.TEST_NEW))
         for task_mock in nlp_service.CELERY_NLP_PIPELINE[1:]:
-            self.assertIn(call(task_mock.s()), mock_chain.link.mock_calls)
-        mock_chain.link.assert_called_with(mock_publish_task.s())
+            task_mock.s().link.assert_called_once()
+        mock_chain.link.assert_called_with(hydrate_task_mock.s())
         mock_chain.delay.assert_called_once()
