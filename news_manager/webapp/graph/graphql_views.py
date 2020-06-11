ca6dac963995bb523cbb749f9c6def231af444b4
DeejayRevok <seryi_one@hotmail.com>
2020-06-11 22:35:41 +0200

    Add JWT authentication to graphql base resolvers

--- news_manager/webapp/graph/graphql_views.py
+++ news_manager/webapp/graph/graphql_views.py
@@ -12,6 +12,162 @@ from news_manager.webapp.graph.queries import Query
 
 schema = Schema(query=Query)
 
+GRAPHIQL_JWT_TEMPLATE = """<!--
+The request to this GraphQL server provided the header "Accept: text/html"
+and as a result has been presented GraphiQL - an in-browser IDE for
+exploring GraphQL.
+If you wish to receive JSON, provide the header "Accept: application/json" or
+add "&raw" to the end of the URL within a browser.
+-->
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    html, body {
+      height: 100%;
+      margin: 0;
+      overflow: hidden;
+      width: 100%;
+    }
+    #graphiql {
+      height: 100vh;
+    }
+    .jwt-token {
+      background: linear-gradient(#f7f7f7, #e2e2e2);
+      border-bottom: 1px solid #d0d0d0;
+      font-family: system, -apple-system, 'San Francisco', '.SFNSDisplay-Regular', 'Segoe UI', Segoe, 'Segoe WP', 'Helvetica Neue', helvetica, 'Lucida Grande', arial, sans-serif;
+      padding: 7px 14px 6px;
+      font-size: 14px;
+    }
+  </style>
+  <meta name="referrer" content="no-referrer">
+  <link href="//cdn.jsdelivr.net/npm/graphiql@{{graphiql_version}}/graphiql.css" rel="stylesheet" />
+  <script src="//cdn.jsdelivr.net/gh/github/fetch@3.0.0/fetch.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/react@16.12.0/umd/react.production.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/react-dom@16.12.0/umd/react-dom.production.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/graphiql@{{graphiql_version}}/graphiql.min.js"></script>
+  <script src="//cdn.jsdelivr.net/npm/subscriptions-transport-ws@0.9.16/browser/client.js"></script>
+  <script src="//cdn.jsdelivr.net//npm/graphiql-subscriptions-fetcher@0.0.2/browser/client.js"></script>
+</head>
+<body>
+  <div class="jwt-token">JWT Token <input id="jwt-token" placeholder="JWT Token goes here"></div>
+  <div id="graphiql">Loading...</div>
+  <script>
+    // Collect the URL parameters
+    var parameters = {};
+    window.location.search.substr(1).split('&').forEach(function (entry) {
+      var eq = entry.indexOf('=');
+      if (eq >= 0) {
+        parameters[decodeURIComponent(entry.slice(0, eq))] =
+          decodeURIComponent(entry.slice(eq + 1));
+      }
+    });
+
+    // Produce a Location query string from a parameter object.
+    function locationQuery(params) {
+      return '?' + Object.keys(params).map(function (key) {
+        return encodeURIComponent(key) + '=' +
+          encodeURIComponent(params[key]);
+      }).join('&');
+    }
+
+    // Derive a fetch URL from the current URL, sans the GraphQL parameters.
+    var graphqlParamNames = {
+      query: true,
+      variables: true,
+      operationName: true
+    };
+
+    var otherParams = {};
+    for (var k in parameters) {
+      if (parameters.hasOwnProperty(k) && graphqlParamNames[k] !== true) {
+        otherParams[k] = parameters[k];
+      }
+    }
+
+    var subscriptionsFetcher;
+    if ('{{subscriptions}}') {
+      const subscriptionsClient = new SubscriptionsTransportWs.SubscriptionClient(
+        '{{ subscriptions }}',
+        {reconnect: true}
+      );
+
+      subscriptionsFetcher = GraphiQLSubscriptionsFetcher.graphQLFetcher(
+        subscriptionsClient,
+        graphQLFetcher
+      );
+    }
+
+    var fetchURL = locationQuery(otherParams);
+    // Defines a GraphQL fetcher using the fetch API.
+    function graphQLFetcher(graphQLParams) {
+      const jwtToken = document.getElementById('jwt-token').value;
+      let headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      };
+      if (jwtToken) {
+        headers = {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'X-API-Key': jwtToken ? `Bearer ${jwtToken}` : null
+        };
+      }
+      return fetch(fetchURL, {
+        method: 'post',
+        headers: headers,
+        body: JSON.stringify(graphQLParams),
+        credentials: 'include',
+      }).then(function (response) {
+        return response.text();
+      }).then(function (responseBody) {
+        try {
+          return JSON.parse(responseBody);
+        } catch (error) {
+          return responseBody;
+        }
+      });
+    }
+
+    // When the query and variables string is edited, update the URL bar so
+    // that it can be easily shared.
+    function onEditQuery(newQuery) {
+      parameters.query = newQuery;
+      updateURL();
+    }
+
+    function onEditVariables(newVariables) {
+      parameters.variables = newVariables;
+      updateURL();
+    }
+
+    function onEditOperationName(newOperationName) {
+      parameters.operationName = newOperationName;
+      updateURL();
+    }
+
+    function updateURL() {
+      history.replaceState(null, null, locationQuery(parameters));
+    }
+
+    // Render <GraphiQL /> into the body.
+    ReactDOM.render(
+      React.createElement(GraphiQL, {
+        fetcher: subscriptionsFetcher || graphQLFetcher,
+        onEditQuery: onEditQuery,
+        onEditVariables: onEditVariables,
+        onEditOperationName: onEditOperationName,
+        query: {{query|tojson}},
+        response: {{result|tojson}},
+        variables: {{variables|tojson}},
+        operationName: {{operation_name|tojson}},
+      }),
+      document.getElementById('graphiql')
+    );
+  </script>
+</body>
+</html>"""
+
 
 def setup_routes(app: Application):
     """
@@ -24,6 +180,7 @@ def setup_routes(app: Application):
     GraphQLView.attach(app,
                        schema=schema,
                        graphiql=True,
+                       graphiql_template=GRAPHIQL_JWT_TEMPLATE,
                        route_path='/graphql',
                        executor=AsyncioExecutor(loop=asyncio.get_event_loop()),
                        enable_async=True)
